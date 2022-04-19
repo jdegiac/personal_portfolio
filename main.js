5441e4b38e36892d1b59ecd240395ef40246314a
Jordan DeGiacomo <jdegiacomo@suffolk.edu>
2022-04-19 00:50:52 -0400

    removed hamburger menu

--- main.js
+++ main.js
@@ -13,20 +13,6 @@ window.addEventListener('scroll', () => {
     updateList();
 })
 
-function myFunction() {
-	var moreText = document.getElementById("more");
-	var btnText = document.getElementById("myBtn");
-
-	if (moreText.style.display === "inline") {
-		btnText.innerHTML = "Read More";
-		moreText.style.display = "none";
-	}
-	else{
-		btnText.innerHTML = "Read less"; 
-	  	moreText.style.display = "inline";
-	}
-  }
-
 function myFunc(btn_id) {
   
 	var btnText = document.getElementById(btn_id.id);
